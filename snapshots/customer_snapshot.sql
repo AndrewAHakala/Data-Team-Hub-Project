3f2b5bb9e325b00595006ef3354b61827339afe4
Andrew Hakala <andrew.hakala@fishtownanalytics.com>
2025-03-20 13:55:47 +0000

    fixing incremental models

--- snapshots/customer_snapshot.sql
+++ snapshots/customer_snapshot.sql
@@ -1,10 +1,10 @@
 --Snapshots are used to capture point-in-time states of a source table, tracking changes to records over time
 --snapshot strategy timestamp or check
-/* {% snapshot customer_snapshot %}
+{% snapshot customer_snapshot %}
 {{
     config(
         target_schema='snapshots',
-       unique_key='id',
+       unique_key='customer_id',
        strategy='timestamp',
         updated_at='modified_timestamp'
     )
@@ -16,4 +16,3 @@ select
     modified_timestamp
 from {{ source('customer_data_source', 'customer_dim') }}
 {% endsnapshot %}
-*/
