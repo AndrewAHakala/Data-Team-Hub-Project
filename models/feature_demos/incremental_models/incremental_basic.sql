12fdf6d5689c23d436c3cb684415877463f2e9f2
Andrew Hakala <andrew.hakala@fishtownanalytics.com>
2025-03-28 17:58:30 +0000

    adding snap and inc

--- models/feature_demos/incremental_models/incremental_basic.sql
+++ models/feature_demos/incremental_models/incremental_basic.sql
@@ -1,27 +1,22 @@
 --Add a new row to preserve history, typically with start/end dates or a version number to track changes.
 --Table persists and grows over time as new data is added.
 --Ideal for large, append-only datasets or event-based data where reprocessing everything would be inefficient.
-
+--dbt run-operation add_row_to_example_orders_table
 {{
     config(
-        materialized='incremental',
-        unique_key='customer_id'
+        materialized='incremental'
+        ,unique_key='id'
     )
 }}
 
-with t as (
 select
-    id as customer_id,
-    concat(fname,' ',lname) as customer_name,
-    address, 
-    modified_timestamp,
-    current_timestamp() as valid_from,
-    null as valid_to
-from {{ source('customer_data_source', 'customer_dim') }})
+    id,
+    status,
+    CAST(CREATED_TIMESTAMP AS TIMESTAMP_LTZ(6)) AS CREATED_TIMESTAMP,
+    CAST(MODIFIED_TIMESTAMP AS TIMESTAMP_LTZ(6)) AS MODIFIED_TIMESTAMP
+from {{ source('example_source', 'example_orders_table') }}
 
-select *
-from t
 {% if is_incremental() %}
 --identify new or updated records
-where modified_timestamp > (select max(modified_timestamp) from t)
+where modified_timestamp > (select max(modified_timestamp) from {{ this }})
 {% endif %}
