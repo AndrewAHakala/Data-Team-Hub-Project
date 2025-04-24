fce5dc7d20165606f9ea8b4fc3e5290edc4fb36b
Andrew Hakala <andrew.hakala@fishtownanalytics.com>
2025-04-24 17:32:31 +0000

    Add new model and hard code revenue

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -4,12 +4,13 @@
     )
 }}
 select
+    order_item_key,
     date_trunc('month', order_date) as order_month,
     region_name,
     100 as gross_revenue
 
 from {{ ref('fct_order_items') }}
 group by 
-    1, 2
+    1, 2,3
 order by 
-    1, 2
+    1, 2,3
