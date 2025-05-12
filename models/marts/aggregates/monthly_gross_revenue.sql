224b8f82fb56f5fb90116149bdecc2882428fcfb
Andrew Hakala <andrew.hakala@fishtownanalytics.com>
2025-05-12 13:50:07 +0000

    update monthly gross revenue

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -4,13 +4,12 @@
     )
 }}
 select
-    order_item_key,
     date_trunc('month', order_date) as order_month,
     region_name,
-    1000 as gross_revenue
+    sum(gross_item_sales_amount) as gross_revenue
 
 from {{ ref('fct_order_items') }}
 group by 
-    1, 2,3,4
+    1, 2
 order by 
-    1, 2,3,4
+    1, 2
