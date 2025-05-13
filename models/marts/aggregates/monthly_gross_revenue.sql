ff4e392af6398e4a9bb3d9a73c9103ccf592d34a
Andrew Hakala <andrew.hakala@fishtownanalytics.com>
2025-05-13 17:02:50 +0000

    chjanged to daily

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -4,12 +4,13 @@
     )
 }}
 select
-    date_trunc('month', order_date) as order_month,
+    date_trunc('day', order_date) as order_day,
     region_name,
-    sum(gross_item_sales_amount) as gross_revenue
+    sum(gross_item_sales_amount) as gross_revenue 
+    from {{ ref('fct_order_items') }}
 
-from {{ ref('fct_order_items') }}
-group by 
+
+group by
     1, 2
-order by 
+order by
     1, 2
