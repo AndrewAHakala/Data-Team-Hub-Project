24650a59c423509f19a8dc2ecd33a77193965dbd
ANDREW HAKALA <andrewhakala1@gmail.com>
2025-05-30 16:15:16 -0400

    ..

--- models/samples/materialized_view/test_materialized_view.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='materialized_view') }}
-
-SELECT 
-    RECEIPT_DATE,
-    SUM(base_price) AS total_sales
-FROM {{ ref('order_items') }}
-GROUP BY 1
--- models/samples/python/post_model.sql
+++
@@ -1,4 +0,0 @@
-Select 
-*,
-2 as another_new_column
-from {{ ref('python_model') }}
