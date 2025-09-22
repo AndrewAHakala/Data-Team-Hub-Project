4689c9610b5ea78023860207fe3b55b8f865b75c
Andrew Hakala <andrew.hakala@fishtownanalytics.com>
2025-09-22 11:01:42 +0000

    quick fix

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,15 +1,9 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+{{ config(materialized="table") }}
 select
-    date_trunc('month', order_date) as order_month,
+    date_trunc('month', order_date) as order_months,
     region_name,
     sum(gross_item_sales_amount) as gross_revenue
 
-from {{ ref('fct_order_items') }}
-group by 
-    order_month, region_name
-order by 
-    order_month, region_name
+from {{ ref("fct_order_items") }}
+group by order_months, region_name
+order by order_months, region_name
