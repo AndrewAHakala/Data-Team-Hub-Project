be7bf47ec126a44b1a6c934a892fe6fc2eaec525
AndrewHakala <45984071+AndrewAHakala@users.noreply.github.com>
2025-10-24 15:06:11 -0400

    Auto commit for Studio handoff

---
+++ models/marts/aggregates/monthly_gross_revenue_83859.sql
@@ -0,0 +1,35 @@
+WITH fct_order_items AS (
+  SELECT
+    *
+  FROM {{ ref('fct_order_items') }}
+), formula_1 AS (
+  SELECT
+    *,
+    DATE_TRUNC('MONTH', ORDER_DATE) AS ORDER_MONTH
+  FROM fct_order_items
+), aggregate_1 AS (
+  SELECT
+    ORDER_MONTH,
+    REGION_NAME,
+    SUM(GROSS_ITEM_SALES_AMOUNT) AS GROSS_REVENUE
+  FROM formula_1
+  GROUP BY
+    ORDER_MONTH,
+    REGION_NAME
+), order_1 AS (
+  SELECT
+    *
+  FROM aggregate_1
+  ORDER BY
+    ORDER_MONTH ASC,
+    REGION_NAME ASC
+), monthly_gross_revenue_83859_sql AS (
+  SELECT
+    ORDER_MONTH,
+    REGION_NAME,
+    GROSS_REVENUE
+  FROM order_1
+)
+SELECT
+  *
+FROM monthly_gross_revenue_83859_sql
