8081bf675b0730e763197a65a4053d9ae6d77d7a
Andrew Hakala <andrew.hakala@dbtlabs.com>
2026-02-12 19:31:05 +0000

    ..

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,6 +1,6 @@
 {{ config(materialized="table") }}
 select
-    date_trunc('day', order_date) as order_month,
+    date_trunc('month', order_date) as order_month,
     region_name,
     sum(gross_item_sales_amount) as gross_revenue
 
