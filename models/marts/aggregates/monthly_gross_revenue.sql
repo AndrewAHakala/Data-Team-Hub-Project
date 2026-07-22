7ece943b3dd9483bb55c3f3d677deedcdd3f491b
Andrew Hakala <andrew.hakala@dbtlabs.com>
2026-07-22 18:02:37 +0000

    Hard code region name to Europe

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,7 +1,8 @@
 {{ config(materialized="table") }}
 select
     date_trunc('month', order_date) as order_month,
-    region_name,
+    --region_name,
+    'EUROPE' as region_name,
     sum(gross_item_sales_amount) as gross_revenue
 
 from {{ ref("fct_order_items") }}
